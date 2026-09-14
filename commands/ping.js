28e5332a83c34f7c020086da650436769cf60f28
kolade199 <broooooohhoo@gmail.com>
2026-09-14 23:45:34 +0100

    Simplify bot info message format
    Removed decorative borders from bot info message.

--- commands/ping.js
+++ commands/ping.js
@@ -29,11 +29,10 @@ async function pingCommand(sock, chatId, message) {
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
         const botInfo = `
-┏━━〔 🤖 𝐊𝐧𝐢𝐠𝐡𝐭𝐁𝐨𝐭-𝐌𝐃 〕━━┓
-┃ 🚀 Ping     : ${ping} ms
-┃ ⏱️ Uptime   : ${uptimeFormatted}
-┃ 🔖 Version  : v${settings.version}
-┗━━━━━━━━━━━━━━━━━━━┛`.trim();
+
+        Ping     : ${ping} ms
+
+
 
         // Reply to the original message with the bot info
         await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
